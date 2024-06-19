2bc5025be7bc7dee5d43573e463e162b1c7f17dc
mike-00 <maixuanytp3@gmail.com>
2024-06-19 20:50:52 +0700

    UPDATE INIT

--- backend/routes/api/admin.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const dashboardController = require("../../controller/dashboard.js");
-
-router.get("/", dashboardController.getIndexPage);
-
-module.exports = router;
--- backend/routes/api/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../../controller/auth.js");
-
-router.get("/sign-up", authController.getPageRegister);
-router.get("/sign-in", authController.getPageLogin);
-router.get("/forgot-password", authController.getPageForgotPassword);	
-
-module.exports = router;
