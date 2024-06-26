21361d53b8606779beaa97a84cb7a639bfd8651a
mike-00 <maixuanytp3@gmail.com>
2024-06-26 17:30:43 +0700

    Udate

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,91 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const device = require('express-device');
+const requestIp = require('request-ip');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const httpStatus = require('http-status');
+const mongoURI = process.env.MONGODB_URI;
+const otherHelper = require('./helper/others.helper');
+const app = express();
+// Logger middleware
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(device.capture());
+// Body parser middleware
+// create application/json parser
+app.use(
+  bodyParser.json({
+    limit: '50mb',
+  }),
+);
+// create application/x-www-form-urlencoded parser
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: false,
+  }),
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// DB Config
+mongoose.Promise = global.Promise;
 
+Promise.resolve(app)
+  .then(MongoDBConnection())
+  .catch(err => console.error.bind(console, `MongoDB connection error: ${JSON.stringify(err)}`));
+
+// Database Connection
+async function MongoDBConnection() {
+  console.log(`| MongoDB URL  : ${mongoURI}`);
+  await mongoose
+    .connect(mongoURI)
+    .then(() => {
+      console.log('| MongoDB Connected');
+      console.log('|--------------------------------------------');
+    });
+
+  return null;
+}
+
+// CORS setup for dev
+app.use(function (req, res, next) {
+  req.client_ip_address = requestIp.getClientIp(req);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'DELETE, GET, POST, PUT, PATCH');
+  next();
+});
+
+
+const routes = require('./routes/index');
+// Use Routes
+app.use('/api', routes);
+app.use('/public', express.static(path.join(__dirname, 'public')));
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// no stacktraces leaked to user unless in development environment
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    return otherHelper.sendResponse(res, httpStatus.NOT_FOUND, false, null, err, 'Route Not Found', null);
+  } else {
+    console.log('\x1b[41m', err);
+    let path = req.baseUrl + req.route && req.route.path;
+    if (path.substr(path.length - 1) === '/') {
+      path = path.slice(0, path.length - 1);
+    }
+    err.method = req.method;
+    err.path = req.path;
+    AddErrorToLogs(req, res, next, err);
+    return otherHelper.sendResponse(res, httpStatus.INTERNAL_SERVER_ERROR, false, null, err, null, null);
+  }
 });
 
 module.exports = app;
